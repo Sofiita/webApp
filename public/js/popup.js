4dbce71b52b6fb7559fb3a6a2fcccb1a86483a9f
Sofia Heit <heitdutra@gmail.com>
2013-09-04 14:15:35 -0300

    fix popup

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,14 +1,26 @@
-$(document) .ready (function () {
-	$('#boton') .click (function) {
-		$('backgPopup') .css ( 'display,block');
-			$('popUp') .css ( 'display,black');
+$(document).ready(function () {
+    $("a#registrarse").on('click', function () {
+        $("#bgpopUp").fadeIn(500, function () {
+            $("#bgpopUp").css('display', 'block');
+            $('#registrarme').fadeIn(500);
+            window.scrollTo(0, 0);
+            disableScroll()
 		});
-			});
+    });
 
-	$('#cerrar') .click (function () {
-		$('#backgPopup') .fadeout (slow);
-			$('#popUp') .fadeout (slow);
-		});
-			});
+	$('#cerrar').on('click', function () {
+		$('#bgpopUp').fadeOut(500);
+		$('#registrarme').fadeOut(500);
+		enableScroll();
+	});
 
+});
 
+function disableScroll() {
+	window.scrollBackup = window.onscroll;
+	window.onscroll = function () { window.scrollTo(0, 0); };
+}
+
+function enableScroll() {
+	window.onscroll = window.scrollBackup;
+}
